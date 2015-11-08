0bba444c264dd6ea8584474b5d44eb17eba63bba
SehrishRaza <smilingsehree@gmail.com>
2015-11-08 20:39:17 +0000

    Logger added

--- src/Scenarios/ScenarioCheckBox.java
+++ src/Scenarios/ScenarioCheckBox.java
@@ -5,12 +5,17 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.logging.FileHandler;
 
 /**
  * Created by SehrishRaza on 07/11/2015.
  */
 public class ScenarioCheckBox {
     private WebDriver driver;
+    Logger logger = LoggerFactory.getLogger(ScenarioCheckBox.class);
 
     @Before
     public void setUp() {
@@ -28,9 +33,11 @@ public class ScenarioCheckBox {
         CheckBox checkBox = new CheckBox(driver);
         checkBox.selectCheckBox(SelectCheckBoxDays.DAYS_MONDAY);
         Assert.assertTrue(checkBox.isSelected(SelectCheckBoxDays.DAYS_MONDAY));
+        logger.info("Monday is clicked");
         //System.out.println("Monday is selected");
         checkBox.selectCheckBox(SelectCheckBoxDays.DAYS_SUNDAY);
         Assert.assertTrue(checkBox.isDisabled(SelectCheckBoxDays.DAYS_SUNDAY));
+        logger.info("Sunday is disable");
         //System.out.println("Sunday is Disabled");
     }
 
